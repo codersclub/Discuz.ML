c9a971e9d9dc6aad219c9803ab14ead7b8f674ba
vvotintsev@gmail.com <vvotintsev@gmail.com>
2013-03-06 21:55:58 +0400

    Created placeholders for our existing language packs

---
+++ upload/source/language/sc/forum/lang_swfupload.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_swfupload.php 27449 2012-02-01 05:32:35Z zhangguosheng $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = "
+<okbtn>纭?畾</okbtn>
+<ctnbtn>缁х画</ctnbtn>
+<fileName>鏂囦欢鍚岦/fileName>
+<size>鏂囦欢澶у皬</size>
+<stat>涓婁紶杩涘害</stat>
+<browser>娴忚?</browser>
+<delete>鍒犻櫎</delete>
+<return>杩斿洖</return>
+<upload>涓婁紶</upload>
+<okTitle>涓婁紶瀹屾垚</okTitle>
+<okMsg>鏂囦欢涓婁紶瀹屾垚</okMsg>
+<uploadTitle>姝ｅ湪涓婁紶</uploadTitle>
+<uploadMsg1>鎬诲叡鏈堻/uploadMsg1>
+<uploadMsg2>涓?枃浠剁瓑寰呬笂浼?姝ｅ湪涓婁紶绗?/uploadMsg2>
+<uploadMsg3>涓?枃浠迭/uploadMsg3>
+<bigFile>鏂囦欢杩囧ぇ</bigFile>
+<uploaderror>涓婁紶澶辫触</uploaderror>
+";
+
+?>
