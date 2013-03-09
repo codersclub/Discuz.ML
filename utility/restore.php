5a29efc791b5a28c82c228a24127565ac6797f85
vvotintsev@gmail.com <vvotintsev@gmail.com>
2013-03-09 17:51:11 +0400

    utility/restore.php synced with x2.5ML

--- utility/restore.php
+++ utility/restore.php
@@ -5,6 +5,7 @@
  *      This is NOT a freeware, use is subject to license terms
  *
  *      $Id: restore.php 22782 2011-05-20 09:39:07Z svn_project_zhangjie $
+ *	Modified by Valery Votintsev, codersclub.org
  */
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
@@ -17,6 +18,7 @@ define('ROOT_PATH', dirname(__FILE__).'/../');
 @include_once ROOT_PATH.'./config/config_global.php';
 define('CHARSET', $_config['output']['charset']);
 define('DBCHARSET', $_config['db']['1']['dbcharset']);
+/*vot*/ define('DISCUZ_LANG', $_config['output']['language']);
 
 $lock_file = ROOT_PATH.'./data/restore.lock';
 if(file_exists($lock_file)) {
@@ -401,12 +403,14 @@ function sizecount($size) {
 function show_header() {
 	ob_start();
 	$charset = CHARSET;
+/*vot*/	$title = lang('restore_title');//vot
+/*vot*/	$intro = lang('restore_questions');//vot
 	print <<< EOT
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=$charset" />
-<title>Discuz! 数据恢复工具</title>
+<!--vot--><title>$title</title>
 <style type="text/css">
 * { word-break: break-all; }
 body { text-align:center; margin:0; padding:0; background: #F5FBFF; font-size:12px; font-family:Verdana, Arial, Helvetica, 'SimSun', sans-serif; line-height: 1.8; }
@@ -455,8 +459,8 @@ table { border-collapse:collapse; margin-bottom:20px; }
 
 <div class="bodydiv">
 	<h1>
-		Discuz! 数据恢复工具
-		<span> &nbsp; 恢复当中有任何问题请访问技术支持站点 <a href="http://www.discuz.net" target="_blank">http://www.discuz.net</a></span>
+<!--vot-->	$title
+<!--vot-->	<span> &nbsp; $intro <a href="http://www.discuz.net" target="_blank">http://www.discuz.net</a></span>
 	</h1>
 	<div class="main">
 EOT;
@@ -486,11 +490,11 @@ function show_msg($message, $url_forward = '', $type = 'message', $success = 0) 
 		echo '<span'.($success ? '' : ' class="red"').'>'.$message.'</span>';
 	} elseif($type == 'redirect') {
 		echo "$message ...";
-		echo "<br /><br /><br /><a href=\"$url_forward\">浏览器会自动跳转页面，无需人工干预。除非当您的浏览器长时间没有自动跳转时，请点击这里</a>";
+/*vot*/		echo "<br /><br /><br /><a href=\"$url_forward\">"+lang('browser_jump')+"</a>";
 		echo "<script>setTimeout(\"redirect('$url_forward');\", 1250);</script>";
 	} elseif($type == 'confirm') {
 		echo "$message";
-		echo "<br /><br /><br /><button id=\"confirmbtn\" onclick=\"redirect('$url_forward')\">确定</button><button id=\"cancelbtn\" onclick=\"redirect('{$siteurl}restore.php')\">取消</button>";
+/*vot*/		echo "<br /><br /><br /><button id=\"confirmbtn\" onclick=\"redirect('$url_forward')\">"+lang('ok')+"</button><button id=\"cancelbtn\" onclick=\"redirect('{$siteurl}restore.php')\">"+lang('cancel')+"</button>";
 	}
 
 	show_footer();
@@ -540,45 +544,10 @@ function show_tips($tip, $title = '', $comment = '', $style = 1) {
 }
 
 function lang($lang_key, $force = true, $replace = array()) {
-	$lang = array('filename' => '文件名称',
-				'director' => '所在目录',
-				'version' => '版本',
-				'time' => '备份时间',
-				'type' => '类型',
-				'size' => '尺寸',
-				'db_method' => '方式',
-				'db_volume' => '卷数',
-				'import' => '导入',
-				'different_dbcharset_tablepre' => '检测到导入的备份数据与配置文件的{diff} 不同，您还要继续运行此程序吗？',
-				'db_import_tips' => '本功能在恢复备份数据的同时，将全部覆盖原有数据，请确定恢复前已将论坛关闭，恢复全部完成后可以将论坛重新开放。<br />您可以通过数据备份管理功能查看站点的备份文件的详细信息，删除过期的备份,并导入需要的备份。<br /><span class="red">恢复数据的整个过程会在一个新页面完成，您成功恢复数据后请务必及时删除restore.php文件。</span><br />',
-				'db_export_discuz' => 'Discuz! 数据(不含UCenter)',
-				'db_export_discuz_uc' => 'Discuz! 和 UCenter 数据',
-				'db_multivol' => '多卷',
-				'db_import_unzip' => '解压缩',
-				'db_export_zip' => '压缩备份',
-				'db_zip' => 'ZIP',
-				'db_shell' => 'Shell',
-				'unknown' => '未知',
-				'backup_file_unexist' => '备份文件不存在',
-				'connect_error' => '连接数据库失败，请您查看数据库配置文件config/config_global.php和config/config_ucenter.php是否存在以及配置是否正确',
-				'dbcharsetdiff' => ' 数据库字符集($_config[\'db\'][\'1\'][\'dbcharset\'])',
-				'tableprediff' => ' 表前缀($_config[\'db\'][\'1\'][\'tablepre\'])',
-				'database_import_multivol_succeed' => '分卷数据成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
-				'database_import_file_illegal' => '数据文件不存在：可能服务器不允许上传文件或文件大小超过限制',
-				'database_import_multivol_prompt' => '分卷数据第一卷成功导入数据库，您需要自动导入本次备份的其他分卷吗？',
-				'database_import_succeed' => '数据已成功导入站点数据库<br />请在后台更新缓存<br /><span class="red">请尽快删除restore.php文件，以免对数据造成影响</span>',
-				'database_import_format_illegal' => '数据文件非 Discuz! 格式，无法导入',
-				'database_import_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
-				'database_import_multivol_unzip' => '{info}<br />备份文件解压缩完毕，您需要自动解压缩其他的分卷文件吗？',
-				'database_import_multivol_unzip_redirect' => '数据文件 #{multivol} 解压缩成功，程序将自动继续',
-				'database_import_confirm' => '导入和当前 Discuz! 版本不一致的数据极有可能产生无法解决的故障，您确定继续吗？',
-				'database_import_confirm_sql' => '您确定导入该备份吗？',
-				'database_import_confirm_zip' => '您确定解压该备份吗？',
-				'database_import_multivol_confirm' => '所有分卷文件解压缩完毕，您需要自动导入备份吗？导入后解压缩的文件将会被删除',
-				'database_import_multivol_redirect' => '数据文件 #{volume} 成功导入，程序将自动继续',
-				'error_quit_msg' => '必须解决以上问题，才能继续恢复数据',
-				'restored_error' => '恢复数据功能锁定，已经恢复过了，如果您确定要恢复数据，请到服务器上删除./data/restore.lock',
-			);
+//vot MOVED TO lang_restore.php:	$lang = array('filename' => '文件名称',...
+/*vot*/	static $lang;
+/*vot*/	require_once ROOT_PATH.'source/language/'.DISCUZ_LANG.'/lang_restore.php';
+
 	$return = isset($lang[$lang_key]) ? $lang[$lang_key] : ($force ? $lang_key : '');
 	if($replace && is_array($replace)) {
 		$searchs = $replaces = array();
