d79e19f72217891cc02b79742caf3a5db4d8e6f7
vvotintsev@gmail.com <vvotintsev@gmail.com>
2013-03-16 15:37:23 +0400

    Added module/misc/misc_googlemap.php

---
+++ upload/source/module/misc/misc_googlemap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: misc_googlemap.php by Valery Votintsev $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+//DEBUG
+//echo "source/module/misc/misc_googlemap.php<br>";
+
+include template('common/googlemap');
+
+
+
