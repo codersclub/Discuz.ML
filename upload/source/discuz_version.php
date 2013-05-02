4dcdf0db60971c9e239812312aa54bfe1150ba86
vvotintsev@gmail.com <vvotintsev@gmail.com>
2013-05-02 19:12:32 +0400

    Fix for language/en/forum/lang_template.php

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -16,7 +16,7 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3 RC');
 	define('DISCUZ_RELEASE', '20130422');
 	define('DISCUZ_FIXBUG', '30000000');
-	define('DISCUZ_ML_REVISION', '103');
+	define('DISCUZ_ML_REVISION', '104');
 }
 
 ?>
