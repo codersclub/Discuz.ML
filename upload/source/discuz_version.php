2bd5fe9636e79e2c3fc8f9f4c4ec25d6be7cbd7b
vvotintsev@gmail.com <vvotintsev@gmail.com>
2013-05-22 08:24:57 +0400

    Fixed bug in template/footer

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -16,7 +16,7 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3');
 	define('DISCUZ_RELEASE', '20130511');
 	define('DISCUZ_FIXBUG', '30000000');
-	define('DISCUZ_ML_REVISION', '127');
+	define('DISCUZ_ML_REVISION', '128');
 }
 
 ?>
