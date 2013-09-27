dc06d973c35443e59fd6b3233a8ff83376baeb54
codersclub <vvotintsev@gmail.com>
2013-09-27 15:16:26 +0400

    /source/include/ upgraded to release 2013-09-23

--- upload/source/include/post/post_editpost.php
+++ upload/source/include/post/post_editpost.php
@@ -312,7 +312,7 @@ if(!submitcheck('editsubmit')) {
 
 	if(empty($_GET['delete'])) {
 
-/*vot*/		$subject=dstripslashes($subject);
+/*vot*/		$subject = dstripslashes($subject);
 
 		if($isfirstpost) {
 
