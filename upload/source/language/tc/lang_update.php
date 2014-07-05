4fdabafad8362201cee74a347ab87b759e410d30
Valery Votintsev <vvotintsev@gmail.com>
2014-07-05 20:08:08 +0400

    Small fix for language files

--- upload/source/language/tc/lang_update.php
+++ upload/source/language/tc/lang_update.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *      $Id: lang_update.php by Valery Votintsev at codersclub.org
+ *      $Id: lang_update.php by Valery Votintsev, codersclub.org
  */
 
 $lang = array(
