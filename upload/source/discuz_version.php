f82ed77a2d15b3e69d6f0b545bd4e52cea87a9d7
Valery Votintsev <vvotintsev@gmail.com>
2016-07-29 23:02:23 +0300

    Replace "$_G['setting']['reglinkname']" with "{lang register}"

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -16,7 +16,7 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.2');
 	define('DISCUZ_RELEASE', '20160601');
 	define('DISCUZ_FIXBUG', '32000000');
-	define('DISCUZ_ML_REVISION', '478');
+	define('DISCUZ_ML_REVISION', '479');
 }
 
 ?>
