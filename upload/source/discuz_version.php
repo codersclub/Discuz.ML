2bc7b893a088804f4b4571f4a39c526d9696e745
Comsenz <10433182@qq.com>
2017-08-16 06:22:25 +0300

    Optimize temporary storage

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -16,5 +16,5 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
 	define('DISCUZ_RELEASE', '20170801');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_ML_REVISION', '721');
+	define('DISCUZ_ML_REVISION', '724');
 }
