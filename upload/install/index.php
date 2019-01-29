3e67dad78b50901e38c15eac3facfa7f9d309024
Valery Votintsev <vvotintsev@gmail.com>
2019-01-29 21:11:03 +0300

    Fix: Use httpS://addon.discuz.com on install

--- upload/install/index.php
+++ upload/install/index.php
@@ -538,7 +538,7 @@ EOT;
 /*vot*/		echo "</div>\n";
 		echo '<div class="main" style="margin-top: -123px;padding-left:30px"><span id="platformIntro"></span>';
 /*vot*/		echo '<p align="center"><a class="finish" href="'.$default_appurl.'">'.$lang['install_finish'].'</a></p><br /><br />'.$lang['ext_info'].'<hr>';
-		echo '<iframe frameborder="0" width="700" height="550" allowTransparency="true" src="http://addon.discuz.com/api/outer.php?id=installed&siteurl='.urlencode($default_appurl).'&version='.DISCUZ_VERSION.'"></iframe>';
+		echo '<iframe frameborder="0" width="700" height="550" allowTransparency="true" src="https://addon.discuz.com/api/outer.php?id=installed&siteurl='.urlencode($default_appurl).'&version='.DISCUZ_VERSION.'"></iframe>';
 		echo '</div>';
 		show_footer();
 	}
