e39bc3acd18c974b14fb486e963b29bad4d2963d
DiscuzX <153619335@qq.com>
2019-05-29 01:21:58 +0800

    Fix: when the plugin setting link contains an underline character, it is added to the common operation, and the link cannot be opened

--- upload/source/function/function_admincp.php
+++ upload/source/function/function_admincp.php
@@ -199,8 +199,10 @@ function showmenu($key, $menus, $return = 0) {
 	if(is_array($menus)) {
 		foreach($menus as $menu) {
 			if($menu[0] && $menu[1]) {
-				list($action, $operation, $do) = explode('_', $menu[1]);
-				$menu[1] = $action.($operation ? '&operation='.$operation.($do ? '&do='.$do : '') : '');
+				if(strpos($menu[1], 'plugins&operation=config') === false){
+					list($action, $operation, $do) = explode('_', $menu[1]);
+					$menu[1] = $action.($operation ? '&operation='.$operation.($do ? '&do='.$do : '') : '');
+				}
 /*vot*/				$body .= '<li><a href="'.(preg_match('/^(https?:)?\/\//i', $menu[1]) ? $menu[1] : ADMINSCRIPT.'?action='.$menu[1]).'" hidefocus="true" target="'.($menu[2] ? $menu[2] : 'main').'"'.($menu[3] ? $menu[3] : '').'><em onclick="menuNewwin(this)" title="'.cplang('nav_newwin').'"></em>'.cplang($menu[0]).'</a></li>';
 			} elseif($menu[0] && $menu[2]) {
 				if($menu[2] == 1) {
