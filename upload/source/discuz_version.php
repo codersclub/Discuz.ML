838e093bc3007290560abcba55ea81e2b9b8eb32
Valery Votintsev <vvotintsev@gmail.com>
2020-03-11 09:05:56 +0300

    Fix MD5 checksum

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20200301');
+	define('DISCUZ_RELEASE', '20200310');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_ML_REVISION', '1111');
+	define('DISCUZ_ML_REVISION', '1115');
 }
