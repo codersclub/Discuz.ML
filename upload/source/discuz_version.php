15884bdd638bdd4220841690e71e21ff82f0a762
Valery Votintsev <vvotintsev@gmail.com>
2020-09-05 20:27:30 +0300

    Fix MD5 and Discuz Version

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20200507');
+	define('DISCUZ_RELEASE', '20200818');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_ML_REVISION', '1158');
+	define('DISCUZ_ML_REVISION', '1196');
 }
