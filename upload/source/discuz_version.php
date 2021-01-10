5d63cb91b75bc12e6a6af4a15b206ffc885530d8
Valery Votintsev <vvotintsev@gmail.com>
2021-01-10 19:23:56 +0300

    Update Release to 20210111, fix MD5 checksums

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20200818');
+	define('DISCUZ_RELEASE', '20210111');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_ML_REVISION', '1196');
+	define('DISCUZ_ML_REVISION', '1258');
 }
