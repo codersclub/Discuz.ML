83cede8365114d603c86372face06f4b7cb79c89
Lao Zhou Buluo <laozhoubuluo@126.com>
2021-01-31 10:26:09 +0800

    !713 Fix: restrict the last character to _ in the table prefix format, to avoid the table prefix like pre_1 from being generated and causing the program processing errors

--- upload/install/index.php
+++ upload/install/index.php
@@ -347,7 +347,8 @@ if($method == 'show_license') {
 			}
 		}
 
-		if(strpos($tablepre, '.') !== false || intval($tablepre[0])) {
+		// Restrict the last character to _ in the table prefix format, to avoid the table prefix like pre_1 from being generated and causing the program processing errors
+		if(strpos($tablepre, '.') !== false || intval($tablepre[0]) || strrpos($tablepre, '_') !== strlen($tablepre)) {
 			show_msg('tablepre_invalid', $tablepre, 0);
 		}
 
