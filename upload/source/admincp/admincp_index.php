e863bbbabbb4c62fd852e82fb78f4f339ee3b651
DiscuzX <153619335@qq.com>
2021-05-26 17:48:29 +0800

    !1016 Fix admincp related links

--- upload/source/admincp/admincp_index.php
+++ upload/source/admincp/admincp_index.php
@@ -410,8 +410,9 @@ showtablerow('', array('class="vtop td24 lineheight"', 'class="lineheight team"'
 ));
 showtablerow('', array('class="vtop td24 lineheight"', 'class="lineheight team"'), array(
 	cplang('home_dev_thanks'),
-	'<a href="http://www.discuzfans.com" class="lightlink2 smallfont" target="_blank">Discuz! Fans</a>
-  <a href="http://www.discuz.net/home.php?mod=space&uid=122246" class="lightlink2 smallfont" target="_blank">Heyond</a>
+	'<a href="https://gitee.com/Discuz/DiscuzX/contributors?ref=master" class="lightlink2 smallfont" target="_blank">Discuz! X Git Contributors</a>
+	<a href="http://www.discuzfans.com" class="lightlink2 smallfont" target="_blank">Discuz! Fans</a>
+	<a href="http://www.discuz.net/home.php?mod=space&uid=122246" class="lightlink2 smallfont" target="_blank">Heyond</a>
 	<a href="http://www.discuz.net/home.php?mod=space&uid=632268" class="lightlink2 smallfont" target="_blank">JinboWang</a>
 	<a href="http://www.discuz.net/home.php?mod=space&uid=15104" class="lightlink2 smallfont" target="_blank">Redstone</a>
 	<a href="http://www.discuz.net/home.php?mod=space&uid=10407" class="lightlink2 smallfont" target="_blank">Qiang Liu</a>
@@ -441,11 +442,10 @@ showtablerow('', array('class="vtop td24 lineheight"', 'class="lineheight"'), ar
 	'<a href="https://codersclub.org/discuz.ml/" class="lightlink2" target="_blank">Discuz!ML Official Site</a>,
     <a href="https://codersclub.org/discuzx/" class="lightlink2" target="_blank">Discuz!ML Forum</a>,
 	<a href="https://sourceforge.net/projects/discuz-ml/" class="lightlink2" target="_blank">Discuz!ML Repository</a>,
+	<a href="https://gitee.com/Discuz/DiscuzX" class="lightlink2" target="_blank">Discuz! X Git</a>,
 	<a href="https://www.dismall.com/" class="lightlink2" target="_blank">'.cplang('app_discussion').'</a>,
-	<a href="http://www.discuz.net/redirect.php?service" class="lightlink2" target="_blank">'.cplang('purchase_service').'</a>,
-	<a href="http://www.discuz.net/" class="lightlink2" target="_blank">'.cplang('discussion_area').'</a>,
-	<a href="'.ADMINSCRIPT.'?action=cloudaddons" class="lightlink2" target="_blank">'.cplang('app_center').'</a>,
-	<a href="https://gitee.com/Discuz/DiscuzX" class="lightlink2" target="_blank">Discuz! X Git</a>
+	<a href="https://www.discuz.net/" class="lightlink2" target="_blank">'.cplang('discussion_area').'</a>,
+	<a href="'.ADMINSCRIPT.'?action=cloudaddons" class="lightlink2" target="_blank">'.cplang('app_center').'</a>
 '));
 showtablefooter();
 
