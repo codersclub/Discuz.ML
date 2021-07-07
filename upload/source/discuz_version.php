11d2a455842d05fdda4de766be5e6716b83b10ef
Valery Votintsev <vvotintsev@gmail.com>
2021-07-07 17:10:11 +0300

    Update MD5 files

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20210520');
+	define('DISCUZ_RELEASE', '20210707');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_REVISION', '1492');
+	define('DISCUZ_REVISION', '1552');
 }
