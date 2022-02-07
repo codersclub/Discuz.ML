34d89dec1153e49c1c19388326bba1f8f966c66c
Sink Lake <341936031@qq.com>
2022-02-07 12:19:38 +0000

    Fix: small format problem

--- upload/source/include/space/space_profile.php
+++ upload/source/include/space/space_profile.php
@@ -200,7 +200,7 @@ if(!$_G['privacy']) {
 		include_once template("home/space_profile");
 	} else {
 		$_GET['do'] = 'card';
-		if ( helper_access::check_module('follow') ) {
+		if(helper_access::check_module('follow')) {
 			$follow = C::t('home_follow')->fetch_by_uid_followuid($_G['uid'], $space['uid']);//Whether to listen to the other party
 		}        
 		include_once template("home/space_card");
