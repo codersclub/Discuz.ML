33a77c8b565c227fb48c5de5b1adb240dfd8dc36
Valery Votintsev <vvotintsev@gmail.com>
2022-03-30 21:22:52 +0300

    Sync SC/TC languages

---
+++ upload/source/language/tc/payment/lang_type.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_type.php 362 2021-05-17 15:38:43Z Valery Votintsev $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array(
+	'payment_credit' => '積分充值',
+	'payment_invite' => '購買邀請碼',
+);
+
+?>
