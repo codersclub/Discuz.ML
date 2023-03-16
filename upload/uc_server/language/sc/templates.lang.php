b915460bd7e47eb1152789c8b84e1d70bb05c3f5
Valery Votintsev <vvotintsev@gmail.com>
2023-03-16 20:56:41 +0300

    Sync Default/SC lang
    --HG--
    branch : v3.5

--- upload/uc_server/language/sc/templates.lang.php
+++ upload/uc_server/language/sc/templates.lang.php
@@ -5,7 +5,7 @@
 //------------------------------------------------------
 // Based on UCenter 1.6.0, (c) Comsenz.inc, discuz.net
 //------------------------------------------------------
-// Simlified Chinese Language Pack
+// Simplified Chinese Language Pack
 // by Valery Votintsev, codersclub.org
 //------------------------------------------------------
 
@@ -29,7 +29,7 @@ $languages = array(
 	'to' => '到',
 	'menu' => '菜单',
 
-	'copyright' => 'Copyright &copy; 2001-'.date('Y').' Tencent Cloud.',
+	'copyright' => '&copy; 2001-'.date('Y').' <a href="https://code.dismall.com/" target="_blank">Discuz! Team</a>.',
 
 	'founder'=>'UCenter创始人',
 	'login_username'=>'用户名',
