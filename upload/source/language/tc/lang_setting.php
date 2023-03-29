234df4ed40edf461ef448c0b65a92574d22c296a
Valery Votintsev <vvotintsev@gmail.com>
2023-03-29 12:07:35 +0300

    Restore lang_setting (for a future!)
    --HG--
    branch : v3.5

---
+++ upload/source/language/tc/lang_setting.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_setting.php by Valery Votintsev, codersclub.org
+ *
+ *      This file is automatically generate
+ */
+
+$lang = array (
+//---------------------------
+//vot: Titles from install_var.php, pre_common_nav table
+
+	'nav_1'		=> '門戶',//'Portal',
+	'nav_2'		=> '論壇',//'Forum',
+	'nav_3'		=> '群組',//'Groups',
+	'nav_4'		=> '家園',//'Space',
+	'nav_5'		=> '應用',//'Applications',
+	'nav_6'		=> '插件',//'Plugins',
+	'nav_7'		=> '幫助',//'Help',
+	'nav_8'		=> '排行榜',//'RankList',
+	'nav_9'		=> '廣播',//'Follows',
+	'nav_10'	=> '導讀',//'Guide',
+	'nav_11'	=> '淘帖',//'Collections',
+	'nav_12'	=> '日誌',//'Blogs',
+	'nav_13'	=> '相冊',//'Albums',
+	'nav_14'	=> '分享',//'Shares',
+	'nav_15'	=> '記錄',//'Moods',
+
+	'nav_portal'	=> '門戶',//'Portal',
+	'nav_blog'	=> '日誌',//'Blogs',
+	'nav_album'	=> '相冊',//'Albums',
+	'nav_share'	=> '分享',//'Shares',
+	'nav_doing'	=> '記錄',//'Doings',
+	'nav_wall'	=> '留言板',//'Wall',
+	'nav_group'	=> '群組',//'Groups',
+	'nav_ranklist'	=> '排行榜',//'RankList',
+	'nav_follow'	=> '廣播',//'Follows',
+	'nav_guide'	=> '導讀',//'Guide',
+	'nav_collection'	=> '淘帖',//'Collections',
+
+	'nav_stat'	=> '站點統計',//'Statistics',
+	'nav_report'	=> '舉報',//'Reports',
+	'nav_archiver'	=> 'Archive',
+	'nav_mobile'	=> '手機版',//'Mobile',
+	'nav_friend'	=> '好友',//'Friends',
+	'nav_thread'	=> '帖子',//'Threads',
+	'nav_favorite'	=> '收藏',//'Favorites',
+	'nav_magic'	=> '道具',//'Magic',
+	'nav_medal'	=> '勳章',//'Medals',
+	'nav_task'	=> '任務',//'Tasks',
+	'nav_feed'	=> '動態',//'Feeds',
+
+	'nav_sethomepage'	=> '<img src="static/image/common/home.gif" title="設為首頁">',//'Set as Homepage',
+	'nav_setfavorite'	=> '<img src="static/image/common/fav.gif" title="收藏本站">',//'Add to Favorites',
+	'nav_uchome'		=> 'UCHome',//'',
+	'nav_darkroom'		=> '小黑屋',//'Dark room',
+
+	'idcardtype_choice'	=> '身份證\n護照\n駕駛證',//sc:'身份证\n护照\n驾驶证',//'ID\nPassport\nDriving license',
+	'education_choice'	=> '博士\n碩士\n本科\n專科\n中學\n小學\n其它',//sc:'博士\n硕士\n本科\n专科\n中学\n小学\n其它',//'Doctor\nMaster\nBachelor\nSpecialist\nSchool\nPrimary School\nOther',
+	'bloodtype_choice'	=> 'A\nB\nAB\nO\n其它',//sc:'A\nB\nAB\nO\n其它',//'A\nB\nAB\nO\nOther',
+
+// Titles from pre_common_credit_rule table
+	'credit_rule_post'		=> '發表主題',//'Publish post',
+	'credit_rule_reply'		=> '發表回復',//'Reply post',
+	'credit_rule_digest'		=> '加精華',//'Add Digest',
+	'credit_rule_postattach'	=> '上傳附件',//'Upload attachment',
+	'credit_rule_getattach'		=> '下載附件',//'Download attachment',
+	'credit_rule_sendpm'		=> '發短消息',//'Send PM',
+	'credit_rule_search'		=> '搜索',//'Search',//'搜索',
+	'credit_rule_promotion_visit'	=> '訪問推廣',//'Visit Promotion',//'访问推广',
+	'credit_rule_promotion_register'	=> '註冊推廣',//'Register by Promotion',//'注册推广',
+	'credit_rule_tradefinished'	=> '成功交易',//'Trade finished',//'成功交易',
+	'credit_rule_realemail'		=> '郵箱認證',//'E-mail verification',//'邮箱认证',
+	'credit_rule_setavatar'		=> '設置頭像',//'Upload avatar',//'设置头像',
+	'credit_rule_videophoto'	=> '視頻認證',//'Video verification',//'视频认证',
+	'credit_rule_hotinfo'		=> '熱點信息',//'Hot provided',//'热点信息',
+	'credit_rule_daylogin'		=> '每天登錄',//'Daily login',//'每天登录',
+	'credit_rule_visit'		=> '訪問別人空間',//'Visit other space',//'访问别人空间',
+	'credit_rule_poke'		=> '打招呼',//'Greeting',//'打招呼',
+	'credit_rule_guestbook'		=> '留言',//'Write to Guestbook',//'留言',
+	'credit_rule_getguestbook'	=> '被留言',//'Get Guestbook Message',//'被留言',
+	'credit_rule_doing'		=> '發表記錄',//'Publish doing',//'发表记录',
+	'credit_rule_publishblog'	=> '發表日誌',//'Publish Blog',//'发表日志',
+	'credit_rule_joinpoll'		=> '參與投票',//'Poll Vote',//'参与投票',
+	'credit_rule_createshare'	=> '發起分享',//'Create Share',//'发起分享',
+	'credit_rule_comment'		=> '評論',//'Write Comment',//'评论',
+	'credit_rule_getcomment'	=> '被評論',//'Get comment',//'被评论',
+	'credit_rule_installapp'	=> '安裝應用',//'Install application',//'安装应用',
+	'credit_rule_useapp'		=> '使用應用',//'Use application',//'使用应用',
+	'credit_rule_click'		=> '信息表態',//'Rate object',//'信息表态',
+	'credit_rule_modifydomain'	=> '修改域名',//'Modify domain',//'修改域名',
+	'credit_rule_portalcomment'	=> '文章評論',//'Comment Article',//'文章评论',
+	'credit_rule_followedcollection'	=> '淘專輯被訂閱',//'Collection followed',//'淘专辑被订阅',
+
+//	'credit_rule_uploadimage'	=> 'Upload image',//
+//	'credit_rule_report'		=> 'Report abuse',//
+//	'credit_rule_invitefriend'	=> 'Successfully invite friends',//
+//	'credit_rule_register'		=> 'Registration',//
+//	'credit_rule_realname'		=> 'Real name verification',//
+//	'credit_rule_editrealname'	=> 'Modify real name',//
+//	'credit_rule_editrealemail'	=> 'Change verified E-mail',//
+//	'credit_rule_delavatar'		=> 'Delete avatar',//
+
+// Titles from instal_var for EXTCREDITS
+	'extcredits1'		=> '威望',//'Reputation',
+	'extcredits2'		=> '金錢',//'Money',
+	'extcredits3'		=> '貢獻',//'Contribution',
+	'extcredits4'		=> 'extcredits4',
+	'extcredits5'		=> 'extcredits5',
+	'extcredits6'		=> 'extcredits6',
+	'extcredits7'		=> 'extcredits7',
+	'extcredits8'		=> 'extcredits8',
+// Credits Formula Explanation
+	'digestposts'	=> '精華帖數',//'精华帖数',
+	'posts'		=> '發帖數',//'发帖数',
+	'threads'	=> '主題數',//'主题数',
+	'replies'	=> '回帖數',//'回帖数',
+	'oltime'	=> '在線時間(小時)',//'在线时间',
+	'blogs'		=> '日誌數',//'日志数',
+	'friends'	=> '好友數',//'好友数',
+	'albums'	=> '相冊數',//'相册数',
+	'doings'	=> '記錄數',//'记录数',
+	'polls'		=> '投票數',//'投票数',
+	'sharings'	=> '分享數',//'分享数',
+
+//	'nav_'		=> '',//'',
+
+);
