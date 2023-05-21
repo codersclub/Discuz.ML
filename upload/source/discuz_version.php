a91c81ac848dec948784d48b328ee28c871ce578
Valery Votintsev <vvotintsev@gmail.com>
2023-05-21 20:45:25 +0300

    Update GeoIP database to v.2023-05-22
    --HG--
    branch : v3.4ML

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -16,5 +16,5 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
 	define('DISCUZ_RELEASE', '20230315');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_REVISION', '4024');
+	define('DISCUZ_REVISION', '4164');
 }
