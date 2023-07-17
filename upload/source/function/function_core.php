a3040cd7aed4d47cd49a3c07a5b7f4be6fd9d2cc
夏www <mail@in-summer.com>
2023-07-17 02:17:46 +0000

    !2182 修复手机邮件模版缺失问题 * 修复手机邮件模版缺失问题

--- upload/source/function/function_core.php
+++ upload/source/function/function_core.php
@@ -768,6 +768,10 @@ function template($file, $templateid = 0, $tpldir = '', $gettplfile = 0, $primal
 		if(strpos($tpldir, 'plugin') && (file_exists(DISCUZ_ROOT.$mobiletplfile) || file_exists(substr(DISCUZ_ROOT.$mobiletplfile, 0, -4).'.php'))) {
 			$tplfile = $mobiletplfile;
 		} elseif(!file_exists(DISCUZ_ROOT.TPLDIR.'/'.$mobiletplfile) && !file_exists(substr(DISCUZ_ROOT.TPLDIR.'/'.$mobiletplfile, 0, -4).'.php')) {
+			if(strpos($file, $_G['mobiletpl'][IN_MOBILE].'/email/') === 0) {
+				$tplfile = str_replace($_G['mobiletpl'][IN_MOBILE].'/', '', $tplfile);
+				$file = str_replace($_G['mobiletpl'][IN_MOBILE].'/', '', $file);
+			}
 			$mobiletplfile = './template/default/'.$file.'.htm';
 			if(!file_exists(DISCUZ_ROOT.$mobiletplfile) && !$_G['forcemobilemessage']) {
 				$tplfile = str_replace($_G['mobiletpl'][IN_MOBILE].'/', '', $tplfile);
