910e61351dfac639ab1a19bd72c6f3f3f50b9d72
Valery Votintsev <vvotintsev@gmail.com>
2023-10-15 10:15:37 +0300

    Fix the last release v3.4 R20230520
    --HG--
    branch : v3.4ML

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20230315');
+	define('DISCUZ_RELEASE', '20230520');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_REVISION', '4164');
+	define('DISCUZ_REVISION', '4301');
 }
