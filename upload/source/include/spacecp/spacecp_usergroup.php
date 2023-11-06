bda7c1fc5e639a473161feedf85c84a284014c0f
monkeye <monkeye@qq.com>
2023-11-06 17:45:32 +0800

    扩展用户组权限合并的显示

--- upload/source/include/spacecp/spacecp_usergroup.php
+++ upload/source/include/spacecp/spacecp_usergroup.php
@@ -211,7 +211,8 @@ if(in_array($do, array('buy', 'exit'))) {
 						}
 					}
 				}
-				$forumperm[$forum['fid']][$perm] = preg_match("/(^|\t)(".$_G['groupid'].")(\t|$)/", $forum[$perm]) ? 1 : 0;
+				$groupids = array_merge(array($_G['groupid']), explode("\t", $_G['member']['extgroupids']));
+				$forumperm[$forum['fid']][$perm] = preg_match("/(^|\t)(".implode('|', $groupids).")(\t|$)/", $forum[$perm]) ? 1 : 0;
 			} else {
 				$forumperm[$forum['fid']][$perm] = $defaultperm[$_G['groupid'] != 7 ? 1 : 0][$perm];
 			}
