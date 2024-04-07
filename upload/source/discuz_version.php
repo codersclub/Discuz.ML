4cf76a896e6b86f1c3afa25992b3cd75d75efc44
Valery Votintsev <vvotintsev@gmail.com>
2024-04-07 21:29:01 +0300

    Update Geo IP database
    --HG--
    branch : v3.5

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -16,7 +16,7 @@ if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.5');
 	define('DISCUZ_RELEASE', '20231221');
 	define('DISCUZ_FIXBUG', '35000000');
-	define('DISCUZ_ML_REVISION', '4386');
+	define('DISCUZ_ML_REVISION', '4391');
 }
 
 ?>
